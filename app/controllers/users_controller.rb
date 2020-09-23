b6ea6cba625e3de95e78c00a7a79949d9c861d07
zinahalnajjar <59237103+zinahalnajjar@users.noreply.github.com>
2020-09-23 09:34:16 -0400

    Add users listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
-
   def edit
     @user = User.find(params[:id])
   end
-
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -17,21 +24,18 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-
-
   def create
-   @user = User.new(user_params)
-   if @user.save
-     flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
-     redirect_to articles_path
-   else
-     render 'new'
-   end
- end
-
- private
- def user_params
-   params.require(:user).permit(:username, :email, :password)
- end
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
 end
